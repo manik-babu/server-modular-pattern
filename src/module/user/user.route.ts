9a3df827ae89f652c2a0047dd4aafdf173c85290
manik-babu <mdmanikbabu48@gmail.com>
2025-12-04 19:34:25 +0600

    Ready to Production

--- src/module/user/user.route.ts
+++ src/module/user/user.route.ts
@@ -5,7 +5,7 @@ import auth from "../../middleware/auth";
 //? app.use("/users", userRoute);
 const router = Router();
 
-router.get('/', auth, userController.getUser);
+router.get('/', userController.getUser);
 router.get('/:id', userController.getSingleUser);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
