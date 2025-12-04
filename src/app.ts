8bcdebd00b2ccedefd9ff96fd2d4f072bf8db2d9
manik-babu <mdmanikbabu48@gmail.com>
2025-12-04 16:01:40 +0600

    Fix: file structure

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, { NextFunction, Request, Response } from 'express';
+import initDB from './config/db';
+import userRoute from './module/user/user.route';
+import { authRoute } from './module/auth/auth.route';
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+
+// Initializing DB
+initDB().catch((err: any) => console.log(err));
+
+
+app.use('/users', userRoute);
+app.use('/auth', authRoute);
+
+app.use((req: Request, res: Response) => {
+    res.status(404).json({
+        message: "Path not found!",
+        path: req.url,
+        method: req.method
+    })
+});
+
+
+export default app;
